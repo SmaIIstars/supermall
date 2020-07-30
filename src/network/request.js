ff588540d056d0e95ffc2a8fcc57734865171ab8
Small_stars <52775939+SmaIIstars@users.noreply.github.com>
2020-07-30 20:27:35 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ export function request(config) {
   // 1.创建实例
   const instance = axios.create({
     // 注意大写
-    baseURL: "http://152.136.185.210:8000/api/n3",
+    baseURL: "xxx",
     timeout: 5000
   });
 
